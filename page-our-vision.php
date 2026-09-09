1e97fbec1a8a54bec22f595d14fc84c34c5d1861
ishtiaqwpdev <ig020888@gmail.com>
2026-09-09 10:42:15 +0500

    hhhhhh

--- page-our-vision.php
+++ page-our-vision.php
@@ -594,17 +594,13 @@ $vision_together_support_btn_link = bdc_get_acf_link(
         $vision_checklist_html = ob_get_clean();
       }
 
-      $vision_headline_html  = bdc_hero_lines_html(
+      $vision_headline_html = bdc_hero_lines_html(
         array(
           array( 'text' => $vision_hero_title_line_1, 'class' => 'vision-hero__title-line vision-hero__title-line--navy' ),
           array( 'text' => $vision_hero_title_line_2, 'class' => 'vision-hero__title-line vision-hero__title-line--pink' ),
           array( 'text' => $vision_hero_title_line_3, 'class' => 'vision-hero__title-line vision-hero__title-line--navy' ),
         )
       );
-      $vision_headline_html .= '<span class="vision-hero__title-heart" aria-hidden="true">';
-      $vision_headline_html .= '<svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" stroke-width="1.8" aria-hidden="true">';
-      $vision_headline_html .= '<path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />';
-      $vision_headline_html .= '</svg></span>';
 
       get_template_part(
         'template-parts/page-hero',
